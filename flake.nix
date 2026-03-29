169cf95bc74ec9e6ff2a58f3dd3e9c7c67b6f7f7
0xferrous <0xferrous@proton.me>
2026-03-29 10:55:09 +0000

    feat: init my-nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Public Nix building blocks";
+
+  nixConfig = {
+    extra-substituters = [ "https://numtide.cachix.org" ];
+    extra-trusted-public-keys = [
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    llm-agents = {
+      url = "github:numtide/llm-agents.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agent-box = {
+      url = "github:0xferrous/agent-box";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    foundry = {
+      url = "github:shazow/foundry.nix/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agent-images = {
+      url = "github:nothingnesses/agent-images";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      ...
+    }: { };
+}
