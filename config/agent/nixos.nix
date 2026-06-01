207ff748e989f4cf215fa85b54b707f78e125336
0xferrous <0xferrous@proton.me>
2026-06-01 19:34:02 +0000

    feat: nixos image

---
+++ config/agent/nixos.nix
@@ -0,0 +1,98 @@
+{ lib, impermanence, ... }:
+let
+  impermanenceRoot = "/persist";
+  opensshSettings = {
+    AllowStreamLocalForwarding = lib.mkDefault "yes";
+    AllowTcpForwarding = lib.mkDefault "yes";
+    DisableForwarding = lib.mkDefault false;
+    StreamLocalBindUnlink = lib.mkDefault "yes";
+  };
+in
+{
+  imports = [
+    impermanence.nixosModules.impermanence
+  ];
+
+  nix.settings = {
+    substituters = [
+      "http://10.0.2.2:5000?priority=30"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      "nixos-1:TpdALX3FryCxN1I/WG+lhTeme19H/Ka035MJchdsYH4="
+    ];
+  };
+
+  environment.sessionVariables.HARMONIA_CACHE_URL = "http://10.0.2.2:5000";
+
+  services.openssh.settings = opensshSettings;
+
+  services.getty.autologinUser = "agent";
+
+  users.users.agent = {
+    isNormalUser = true;
+    uid = 1000;
+    group = "users";
+    home = "/home/agent";
+    createHome = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  systemd.tmpfiles.rules = [
+    "d /run/user/1000 0700 agent users - -"
+    "d /run/user/1000/gnupg 0700 agent users - -"
+    "r /run/user/1000/gnupg/S.gpg-agent - - - - -"
+  ];
+
+  systemd.services.agentspace-tmpfiles-create = {
+    description = "Create agentspace runtime tmpfiles";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-tmpfiles-setup.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/systemd/bin/systemd-tmpfiles --create";
+    };
+  };
+
+  boot.loader.grub.enable = false;
+
+  boot.initrd.availableKernelModules = [
+    "virtio_pci"
+    "virtio_blk"
+    "virtiofs"
+    "ext4"
+  ];
+
+  fileSystems."/" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [
+      "mode=0755"
+      "size=2G"
+    ];
+  };
+
+  fileSystems."/nix/store" = {
+    device = "ro-store";
+    fsType = "virtiofs";
+    neededForBoot = true;
+    options = [ "ro" ];
+  };
+
+  fileSystems.${impermanenceRoot} = {
+    device = "/dev/disk/by-label/persist";
+    fsType = "ext4";
+    neededForBoot = true;
+  };
+
+  environment.persistence.${impermanenceRoot} = {
+    directories = [
+      "/var/lib/nixos"
+      "/var/lib/tailscale"
+    ];
+  };
+
+  system.stateVersion = "26.05";
+}
