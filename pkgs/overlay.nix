fd5451da5fc74f07d1339ac6cf80b2912bb65f38
0xferrous <0xferrous@proton.me>
2026-06-06 07:01:01 +0000

    pkgs: add install-bin

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -6,6 +6,7 @@ final: _prev: {
   fr-kbd-backlight = final.callPackage ./keyboard-backlight.nix { };
   git-hunk = final.callPackage ./git-hunk.nix { };
   opensrc = final.callPackage ./opensrc.nix { };
+  "install-bin" = final.callPackage ./install-bin.nix { };
   pi = final.callPackage ./pi.nix {
     piPackage = inputs.llm-agents.packages.${system}.pi;
     agentStuffSrc = inputs."agent-stuff";
