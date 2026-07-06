b207ffd20aeb6d017eebfef15e22c7d1fe5fff4e
0xferrous <0xferrous@proton.me>
2026-07-06 08:22:35 +0000

    fix: fzf integration overrode carapace integration

--- config/fr/home/misc.nix
+++ config/fr/home/misc.nix
@@ -1,6 +1,27 @@
-{ ... }:
 {
-  programs.carapace.enable = true;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  carapaceBin = lib.getExe config.programs.carapace.package;
+in
+{
+  programs.carapace = {
+    enable = true;
+    # Source carapace manually with mkBefore below so fzf's Nushell integration
+    # wraps carapace as its fallback external completer instead of replacing it.
+    enableNushellIntegration = lib.mkForce false;
+  };
+
+  programs.nushell.extraConfig = lib.mkBefore ''
+    source ${
+      pkgs.runCommand "carapace-nushell-config-early.nu" { } ''
+        ${carapaceBin} _carapace nushell | sed 's|"/homeless-shelter|$"($env.HOME)|g' >> "$out"
+      ''
+    }
+  '';
   programs.caelestia = {
     enable = false;
     systemd = {
