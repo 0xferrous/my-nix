29905e369d91c574bbb7d2413864ce61d6681c0a
0xferrous <0xferrous@proton.me>
2026-07-31 16:29:30 +0000

    fix: use Git-aware Ash checkout in nash

--- pkgs/nash.nix
+++ pkgs/nash.nix
@@ -8,7 +8,7 @@ writeShellApplication {
   text = ''
     export ASH_CONFIG_HOME="''${XDG_CONFIG_HOME:-$HOME/.config}/nash"
     export ASH_STATE_HOME="''${XDG_STATE_HOME:-$HOME/.local/state}/nash"
-    exec nix run "path:$HOME/dev/fr/ash" -- "$@"
+    exec nix run "git+file://$HOME/dev/fr/ash" -- "$@"
   '';
   meta.description = "Run the local Ash development checkout with isolated config and state";
 }
